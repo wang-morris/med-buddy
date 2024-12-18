23a49761d343746c47e0c9e37690428c25236310
Morris Wang <morris1214@gmail.com>
2024-12-18 05:38:06 +0000

    patient-form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 export default function App() {
@@ -21,14 +19,7 @@ export default function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <h1>Test</h1>
       <h1>{serverData}</h1>
     </>
   );
