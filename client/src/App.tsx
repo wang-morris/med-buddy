fbc7660f60027fafc8f1a89bcc23d862c3a9fb5e
Morris Wang <morris1214@gmail.com>
2025-01-07 06:02:17 +0000

    patient-form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
 export default function App() {
-  const [serverData, setServerData] = useState('');
-
   useEffect(() => {
     async function readServerData() {
-      const resp = await fetch('/api/hello');
-      const data = await resp.json();
+      try {
+        const resp = await fetch('/api/hello');
+        const data = await resp.json();
 
-      console.log('Data from server:', data);
-
-      setServerData(data.message);
+        console.log('Data from server:', data);
+      } catch (error) {
+        console.error('Error fetching server data:', error);
+      }
     }
 
     readServerData();
@@ -19,8 +19,43 @@ export default function App() {
 
   return (
     <>
-      <h1>Test</h1>
-      <h1>{serverData}</h1>
+      <header>
+        <h1>MedBuddy</h1>
+      </header>
+      <div className="toggle-container">
+        <label className="toggle-item">Patient context</label>
+        <label className="toggle-item">Ask a question</label>
+      </div>
+      <form className="app-form">
+        <div className="form-group">
+          <label form="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Enter your name"
+          />
+        </div>
+        <div className="form-group">
+          <label form="email">Age:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter your email"
+          />
+        </div>
+        <div className="form-group">
+          <label form="message">Date of Birth:</label>
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Enter your message"></textarea>
+        </div>
+        <button type="submit" className="submit-button">
+          Clear Form
+        </button>
+      </form>
     </>
   );
 }
