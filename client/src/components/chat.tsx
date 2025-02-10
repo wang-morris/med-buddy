2ddcb590fdfd108c987488d76981d6469d3a038c
Morris Wang <morris1214@gmail.com>
2025-02-10 04:27:54 +0000

    change views from toggle

---
+++ client/src/components/chat.tsx
@@ -0,0 +1,7 @@
+import './chat.css';
+
+const ChatContainer: React.FC = () => {
+  return <h2>Hello World</h2>;
+};
+
+export default ChatContainer;
--- client/src/components/toggle-bar.tsx
+++ client/src/components/toggle-bar.tsx
@@ -1,30 +1,31 @@
 import { useState } from 'react';
 import './toggle-bar.css';
 
-function ToggleBar() {
+interface ToggleBarProps {
+  setActiveComponent: (component: string) => void;
+}
+
+function ToggleBar({ setActiveComponent }: ToggleBarProps) {
   const [activeButton, setActiveButton] = useState('patient context');
+
+  function getClassName(button: string) {
+    return activeButton === button ? 'active-toggle' : 'inactive-toggle';
+  }
 
   function handleButtonClick(button: string) {
     setActiveButton(button);
+    setActiveComponent(button);
   }
 
   return (
     <div className="toggle-container">
       <button
-        className={`toggle-item ${
-          activeButton === 'patient context'
-            ? 'active-toggle'
-            : 'inactive-toggle'
-        }`}
+        className={`toggle-item ${getClassName('patient context')}`}
         onClick={() => handleButtonClick('patient context')}>
         Patient context
       </button>
       <button
-        className={`toggle-item ${
-          activeButton === 'ask a question'
-            ? 'active-toggle'
-            : 'inactive-toggle'
-        }`}
+        className={`toggle-item ${getClassName('ask a question')}`}
         onClick={() => handleButtonClick('ask a question')}>
         Ask a question
       </button>
