973dd436747819e9b81541c412a7d01788ad03e8
Morris Wang <morris1214@gmail.com>
2025-02-12 05:44:06 +0000

    ensured form data state is passed to all needed components

--- client/src/components/chat.tsx
+++ client/src/components/chat.tsx
@@ -1,7 +1,19 @@
 import './chat.css';
 
-const ChatContainer: React.FC = () => {
-  return <h2>Hello World</h2>;
+type ChatProps = {
+  formData: {
+    sex: string;
+    dob: string;
+    height: { feet: string; inches: string };
+    weight: string;
+    ethnicity: string;
+    additionalInfo: string;
+  };
+};
+
+const ChatContainer = ({ formData }: ChatProps) => {
+  // to test if the form data state is being saved correctly
+  return <p>{JSON.stringify(formData)}</p>;
 };
 
 export default ChatContainer;
