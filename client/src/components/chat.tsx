439ed28b8ae4d88aa131c5c331bd86d749c3631e
Morris Wang <morris1214@gmail.com>
2025-02-17 21:32:43 +0000

    chatbot feature updates

--- client/src/components/chat.tsx
+++ client/src/components/chat.tsx
@@ -1,7 +1,27 @@
 import './chat.css';
 
-const ChatContainer: React.FC = () => {
-  return <h2>Hello World</h2>;
+type ChatProps = {
+  formData: {
+    sex: string;
+    dob: string;
+    height: { feet: string; inches: string };
+    weight: string;
+    ethnicity: string;
+    additionalInfo: string;
+  };
+};
+
+const ChatContainer = ({ formData }: ChatProps) => {
+  return (
+    <footer className="chat-textbox-container">
+      <textarea
+      className="chat-textbox"
+      id="textbox-id"
+      name="textbox-name"
+      placeholder="Ask a medical question about the patient...">
+      </textarea>
+    </footer>
+  )
 };
 
 export default ChatContainer;
